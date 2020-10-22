af2ce699e114c89f0f26ac0d4c2ab508b1644809
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-22 13:18:14 +1100

    basic inquirer and files upload

---
+++ Develop/app.js
@@ -0,0 +1,128 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = role;
+    return;
+  }
+
+  getName() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Enter the Name",
+        },
+      ])
+      .then(function (response) {
+        let name = response.name;
+        console.log(name);
+      });
+  }
+
+  getId() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "id",
+          message: "Enter the ID number",
+        },
+      ])
+      .then(function (response) {
+        let id = response.id;
+        console.log(id);
+      });
+  }
+
+  getEmail() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "email",
+          message: "Enter the email address",
+        },
+      ])
+      .then(function (response) {
+        let email = response.email;
+        console.log(email);
+      });
+  }
+  getRole() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What is the employee's role?",
+          choices: ["Manager", "Engineer", "Intern"],
+          name: "employeeType",
+        },
+      ])
+      .then(function (response) {
+        if (response.employeeType === "Manager") {
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                message: "What is the manager's office number?",
+                name: "officeNumber",
+              },
+            ])
+            .then(function (response) {
+              let officeNumber = response.officeNumber;
+              console.log(officeNumber);
+            });
+        }
+      });
+  }
+}
+
+class Employee extends Manager {
+  constructor(officeNumber) {
+    const officeNumber = officeNumber;
+
+    super(name, id, email, role);
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = role;
+  }
+}
+
+const employee = new Employee();
+employee.getName();
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
