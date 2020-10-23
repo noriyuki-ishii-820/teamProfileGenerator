78416cd3f6c2eca377b3e8377e661cc354546d28
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-23 13:44:28 +1100

    reorganizing the files

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const employee = [];
+const team = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function menu (){
+      function createManager (){
+          inquirer.prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "Enter the Manager's Name",
+            },
+            {
+              type: "input",
+              name: "id",
+              message: "Enter the Manager's ID number",
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "Enter the Manager's email address",
+            },
+
+          ])
+          .then(answers => {
+
+
+          })
+      }
+
+      function createTeam(){
+
+      }
+}
 getName() {
     inquirer
       .prompt([
@@ -27,7 +58,7 @@ getName() {
       });
   }
 
-  getId() {
+getId() {
     inquirer
       .prompt([
         {
