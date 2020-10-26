a7938efec5461a8104eaa8a5678c06cb0d6205bb
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-26 22:14:53 +1100

    demo video added on readme

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { doesNotMatch } = require("assert");
 
 function createManager() {
   inquirer
